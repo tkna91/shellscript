81e3946e54df9ef0788a8e38ef0b2614601f487a
Takahashi, Naoki <102382754+tkna91@users.noreply.github.com>
2023-10-31 11:37:12 +0900

    Update screenshot-upload.sh

--- screenshot-upload.sh
+++ screenshot-upload.sh
@@ -4,5 +4,5 @@ file=~/tmp/$(date "+%Y%m%d-%H%M%S")_screenshot.png
 grim -g "$(slurp)" - | swappy -f - -o - | pngquant - -o "$file" &&
 yad --title="Upload OK?" --text="Upload OK?" --image="dialog-question" --default-cancel &&
 curl -F"file=@$file" https://0x0.st | wl-copy -n &&
-notify-send --expire-time=3000 "Uploaded, URL Copied!." "$(wl-paste)"
+notify-send --expire-time=3000 "Uploaded, URL Copied!." "$(wl-paste)" &&
 mpv --image-display-duration=3 "$(wl-paste)"
